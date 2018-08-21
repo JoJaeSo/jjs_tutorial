b8e53170586c9c7be18b0f0862bc85461ff96714
unknown <784wldnd@gmail.com>
2018-08-21 22:40:39 +0900

    product앱으로 crud 연습

--- Withalcohol/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WithalcoholConfig(AppConfig):
-    name = 'Withalcohol'
--- jjs_tuto/urls.py
+++ jjs_tuto/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from main_index import views as main_views
 
