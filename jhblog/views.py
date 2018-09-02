248a6ba8e190d5e2166c15adef7e7aa7efa690aa
101story <hooraystory@gmail.com>
2018-09-02 18:05:36 +0900

    apply common base.html

--- jhblog/views.py
+++ jhblog/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
-from djnago.template import loader
+from django.template import loader
 
 from .models import Post
 # Create your views here.
 
 def index(request):
     template = loader.get_template('jhblog/index.html')
-
-    return HttpResponse(template.render(context, request))
+    
+    return HttpResponse(template.render(None,request))
